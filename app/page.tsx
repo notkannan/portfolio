1f4c427b2bd99106f4364e4c85ebaa90a8e1c712
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-07-25 19:00:26 +0530

    Update RecentProjects.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@ import Image from "next/image";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full"> 
-        <FloatingNav navItems={[
-          {name: 'Home', link:'/'}
-        ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main> 
   );
