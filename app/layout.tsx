32750f58638d8eae0e05fecd4e8b87abed99a40a
Kannan Karthikeyan <kannan060200@hotmail.com>
2024-07-29 12:08:45 +0530

    Update GAnalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kannan's Portfolio",
-  description: "Modern and Minimalist Portfolio",
+  description: "Kannan Karthikeyan's Portfolio",
 };
 
 export default function RootLayout({
@@ -17,6 +18,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CLJZY2M5RW"></Script>
+        <Script>
+          {
+            `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CLJZY2M5RW');`
+          }
+        </Script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
